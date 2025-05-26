09e9aa2243e52d05ba9b7edf8da0a517f1841fcd
k-haechan <gibbm1127@naver.com>
2025-05-26 15:40:03 +0900

    chore: 프로젝트 초기 설정 (#1)

---
+++ src/main/java/com/mysite/sns_backend/SnsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mysite.sns_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnsBackendApplication.class, args);
+	}
+
+}
