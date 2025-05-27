d43326aab3d0cbe3cc649a992778f17ffd34d60a
k-haechan <gibbm1127@naver.com>
2025-05-27 19:50:00 +0900

    feat: 에러코드를 대응하는 RuntimeException 클래스 정의

---
+++ src/main/java/com/mysite/sns_backend/global/exception/CustomException.java
@@ -0,0 +1,16 @@
+package com.mysite.sns_backend.global.exception;
+
+import com.mysite.sns_backend.global.exception.code.ErrorCode;
+
+import lombok.Getter;
+
+@Getter
+public class CustomException extends RuntimeException {
+	private final com.mysite.sns_backend.global.exception.code.ErrorCode errorCode;
+
+	public CustomException(ErrorCode errorCode) {
+		super(errorCode.getMessage());
+		this.errorCode = errorCode;
+	}
+
+}
