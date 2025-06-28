de26f674838e9ac19ba111a66d66cc07c6ae1ee3
김해찬 <84076757+k-haechan@users.noreply.github.com>
2025-06-28 22:24:24 +0900

    chore : initial 설정 (#2)

---
+++ src/test/java/com/mysite/sns_backend/SnsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mysite.sns_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
